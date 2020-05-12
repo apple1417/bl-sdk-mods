11dc530d494cd5cdbe54cc1872ad718f73d92903
apple1417 <theapple1417@gmail.com>
2020-05-12 14:21:15 +1200

    update alt use vendors

--- AltUseVendors/Readme.md
+++ AltUseVendors/Readme.md
@@ -1,4 +1,8 @@
 ## Changelog
 
+### Alt Use Vendors v1.1
+Made vendor prices only update while a player is near them, which should be more lag friendly.    
+Also added an option to disable vendor price updating entirely - you won't see the prices anymore, but it should be even more lag friendly.
+
 ### Alt Use Vendors v1.0
 Initial Release.
--- AltUseVendors/__init__.py
+++ AltUseVendors/__init__.py
@@ -8,13 +8,18 @@ class AltUseVendors(unrealsdk.BL2MOD):
     Name: ClassVar[str] = "Alt Use Vendors"
     Author: ClassVar[str] = "apple1417"
     Description: ClassVar[str] = (
-        "Adds alt use binds to refill health and ammo at their vendors, like in BL3."
+        "Adds alt use binds to quickly refill health and ammo at their vendors, like in BL3."
     )
     Types: ClassVar[List[unrealsdk.ModTypes]] = [unrealsdk.ModTypes.Utility]
-    Version: ClassVar[str] = "1.0"
+    Version: ClassVar[str] = "1.1"
+
+    UpdatingOption: unrealsdk.Options.Boolean
+    Options: List[unrealsdk.Options.Boolean]
 
     HealthIcon: unrealsdk.UObject
     AmmoIcon: unrealsdk.UObject
+
+    TouchingActors: List[unrealsdk.UObject]
 
     @dataclass
     class _AmmoInfo:
@@ -35,66 +40,117 @@ class AltUseVendors(unrealsdk.BL2MOD):
         "AmmoShop_Sniper_Rifle_Cartridges": _AmmoInfo("Ammo_Sniper_Rifle", 18)
     }
 
+    UpdateDelay: ClassVar[float] = 0.25
+
     def __init__(self) -> None:
         # Hopefully I can remove this in a future SDK update
         self.Author += "\nVersion: " + str(self.Version)  # type: ignore
 
+        self.UpdatingOption = unrealsdk.Options.Boolean(
+            "Updating Costs",
+            "Should the costs of quick buying update live while you're in game. Disabling this"
+            " won't show the costs of quick buying anymore, but may help reduce lag.",
+            True
+        )
+        self.Options = [self.UpdatingOption]
+
         self.HealthIcon = None
         self.AmmoIcon = None
 
+        self.TouchingActors = []
+
     def Enable(self) -> None:
         self.CreateIcons()
-
-        def OnUpdate() -> None:
-            # Can't look for pawns directly due to the streaming ones, which will crash the game
-            allPawns = [PC.Pawn for PC in unrealsdk.FindAll("WillowPlayerController") if PC.Pawn is not None]
-
-            for vendor in unrealsdk.FindAll("WillowVendingMachine"):
-                closetPawn = None
-                minDist = -1
-                for pawn in allPawns:
-                    dist = (
-                        (vendor.Location.X - pawn.Location.X) ** 2
-                        + (vendor.Location.Y - pawn.Location.Y) ** 2  # noqa
-                        + (vendor.Location.Z - pawn.Location.Z) ** 2  # noqa
-                    ) ** 0.5
-                    if dist < minDist or minDist == -1:
-                        dist = minDist
-                        closetPawn = pawn
-
-                if vendor.ShopType != 1 and vendor.ShopType != 2:  # SType_Health
-                    continue
-
-                if vendor.InteractiveObjectDefinition is None:
-                    continue
-
-                cost: int
-                if vendor.ShopType == 2:
-                    vendor.InteractiveObjectDefinition.HUDIconDefSecondary = self.HealthIcon
-                    cost = self.GetHealthCost(closetPawn, vendor)
-                else:
-                    vendor.InteractiveObjectDefinition.HUDIconDefSecondary = self.AmmoIcon
-                    cost = self.GetAmmoCost(closetPawn, vendor)
-                vendor.SetUsability(cost != 0, 1)
-                vendor.Behavior_ChangeUsabilityCost(1, 0, cost, 1)
 
         def ConditionalReactToUse(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
             if params.UsedType != 1:
                 return True
             if str(caller).split(" ")[0] != "WillowVendingMachine":
                 return True
+
+            # If you have updating costs off we have to take the off money ourselves
+            if not self.UpdatingOption.CurrentValue:
+                PC = params.User.Controller
+                PRI = PC.PlayerReplicationInfo
+
+                wallet = PRI.GetCurrencyOnHand(0)
+                cost = 0
+                if caller.ShopType == 2:
+                    cost = self.GetHealthCost(params.User, caller)
+                elif caller.ShopType == 1:
+                    cost = self.GetAmmoCost(params.User, caller)
+                else:
+                    return True
+
+                if cost == 0 or wallet < cost:
+                    # TODO: find something that actually plays a sound or something
+                    caller.NotifyUserCouldNotAffordAttemptedUse(
+                        params.User,
+                        params.UsedComponent,
+                        params.UsedType
+                    )
+                    return True
+
+                PRI.AddCurrencyOnHand(0, -cost)
+                PC.SetPendingTransactionStatus(1)
+
             if caller.ShopType == 2:
                 self.BuyHealth(params.user, caller)
             elif caller.ShopType == 1:
                 self.BuyAmmo(params.user, caller)
+
             return True
 
-        AsyncUtil.RunEvery(0.25, OnUpdate, self.Name)
+        def InitializeFromDefinition(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if str(caller).split(" ")[0] != "WillowVendingMachine":
+                return True
+
+            if caller.ShopType == 2:
+                params.Definition.HUDIconDefSecondary = self.HealthIcon
+                caller.SetUsability(True, 1)
+            elif caller.ShopType == 1:
+                params.Definition.HUDIconDefSecondary = self.AmmoIcon
+                caller.SetUsability(True, 1)
+
+            return True
+
+        def Touch(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if not self.UpdatingOption.CurrentValue:
+                return True
+            if str(caller).split(" ")[0] != "WillowVendingMachine":
+                return True
+
+            self.TouchingActors.append(params.Other)
+            if self.UpdatingOption.CurrentValue and len(self.TouchingActors) == 1:
+                AsyncUtil.RunEvery(self.UpdateDelay, self.OnUpdate, self.Name)
+
+            return True
+
+        def UnTouch(caller: unrealsdk.UObject, function: unrealsdk.UFunction, params: unrealsdk.FStruct) -> bool:
+            if str(caller).split(" ")[0] != "WillowVendingMachine":
+                return True
+
+            try:
+                self.TouchingActors.remove(params.Other)
+            except ValueError:  # If the player is not already in the array
+                pass
+
+            if self.UpdatingOption.CurrentValue and len(self.TouchingActors) == 0:
+                AsyncUtil.CancelFutureCallbacks(self.Name)
+
+            return True
+
         unrealsdk.RunHook("WillowGame.WillowInteractiveObject.ConditionalReactToUse", self.Name, ConditionalReactToUse)
+        unrealsdk.RunHook("WillowGame.WillowInteractiveObject.InitializeFromDefinition", self.Name, InitializeFromDefinition)
+        unrealsdk.RunHook("WillowGame.WillowInteractiveObject.Touch", self.Name, Touch)
+        unrealsdk.RunHook("WillowGame.WillowInteractiveObject.UnTouch", self.Name, UnTouch)
 
     def Disable(self) -> None:
         AsyncUtil.CancelFutureCallbacks(self.Name)
         unrealsdk.RemoveHook("WillowGame.WillowInteractiveObject.ConditionalReactToUse", self.Name)
+        unrealsdk.RemoveHook("WillowGame.WillowInteractiveObject.InitializeFromDefinition", self.Name)
+        unrealsdk.RemoveHook("WillowGame.WillowInteractiveObject.Touch", self.Name)
+        unrealsdk.RemoveHook("WillowGame.WillowInteractiveObject.UnTouch", self.Name)
 
     def CreateIcons(self) -> None:
         if self.HealthIcon is not None and self.AmmoIcon is not None:
@@ -132,6 +188,34 @@ class AltUseVendors(unrealsdk.BL2MOD):
             self.AmmoIcon.Icon = 7
             self.AmmoIcon.Action = "UseSecondary"
             self.AmmoIcon.Text = "REFILL AMMO"
+
+    def OnUpdate(self) -> None:
+        # Can't look for pawns directly due to the streaming ones, which will crash the game
+        allPawns = [PC.Pawn for PC in unrealsdk.FindAll("WillowPlayerController") if PC.Pawn is not None]
+
+        for vendor in unrealsdk.FindAll("WillowVendingMachine"):
+            closetPawn = None
+            minDist = -1
+            for pawn in allPawns:
+                dist = (
+                    (vendor.Location.X - pawn.Location.X) ** 2
+                    + (vendor.Location.Y - pawn.Location.Y) ** 2  # noqa
+                    + (vendor.Location.Z - pawn.Location.Z) ** 2  # noqa
+                ) ** 0.5
+                if dist < minDist or minDist == -1:
+                    dist = minDist
+                    closetPawn = pawn
+
+            cost = 0
+            if vendor.ShopType == 2:
+                cost = self.GetHealthCost(closetPawn, vendor)
+            elif vendor.ShopType == 1:
+                cost = self.GetAmmoCost(closetPawn, vendor)
+            else:
+                continue
+
+            vendor.SetUsability(cost != 0, 1)
+            vendor.Behavior_ChangeUsabilityCost(1, 0, cost, 1)
 
     def GetHealthCost(self, Pawn: unrealsdk.UObject, Vendor: unrealsdk.UObject) -> int:
         if Pawn.GetHealth() == Pawn.GetMaxHealth():
@@ -223,6 +307,22 @@ class AltUseVendors(unrealsdk.BL2MOD):
                 if pool.Definition.Resource.Name == "Ammo_Grenade_Protean":
                     pool.SetCurrentValue(pool.GetMaxValue())
                     break
+
+    def ModOptionChanged(self, Option: unrealsdk.Options.Boolean, newValue: bool) -> None:
+        if Option != self.UpdatingOption:
+            return
+
+        # If you turn on updating and there are people close to vendors, start updating
+        if newValue:
+            if len(self.TouchingActors) > 0:
+                AsyncUtil.RunEvery(self.UpdateDelay, self.OnUpdate, self.Name)
+        # If you turn off updating, stop updating and make sure all vendors are usable at no cost
+        else:
+            AsyncUtil.CancelFutureCallbacks(self.Name)
+            for vendor in unrealsdk.FindAll("WillowVendingMachine"):
+                if vendor.ShopType == 1 or vendor.ShopType == 2:
+                    vendor.SetUsability(True, 1)
+                    vendor.Behavior_ChangeUsabilityCost(1, 0, 0, 1)
 
 
 instance = AltUseVendors()
