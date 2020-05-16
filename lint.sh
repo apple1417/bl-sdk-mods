33c4236b33c0db2328ab3e77b8566fd05181231c
apple1417 <theapple1417@gmail.com>
2020-05-16 14:31:51 +1200

    switch to internal mods dir

--- lint.sh
+++ lint.sh
@@ -1,7 +1,21 @@
 # Need the outer folder to be called 'Mods' like the actual folder
-cd ..
-ln -s bl-sdk-mods Mods
-cd Mods
+mkdir Mods
+
+# Save dirs for later so all ignored ones are logged in one place
+# Symlink everything though so imports still line up
+lintDirs=()
+for mod in */ ; do
+    if [ "$mod" == "Mods/" ]; then
+        continue
+    fi
+
+    ln -s "$PWD/$mod" "$PWD/Mods/${mod%/}"
+    if [ -f $mod/.nolint ]; then
+        echo Not linting \'${mod%/}\'
+        continue
+    fi
+    lintDirs+=( $mod )
+done
 
 # These are files the sdk provides which we might import from
 # Follow imports is off so using empty files works fine
@@ -15,22 +29,12 @@ sdkFiles=(
     "Util.py"
 )
 for file in ${sdkFiles[@]}; do
-    touch $file
-done
-
-# Save dirs for later so all ignored ones are logged in one place
-lintDirs=()
-for mod in */ ; do
-    if [ -f $mod/.nolint ]; then
-        echo Not linting \'${mod%/}\'
-        continue
-    fi
-    lintDirs+=( $mod )
+    touch "Mods/$file"
 done
 
 for mod in ${lintDirs[@]}; do
     echo Checking \'${mod%/}\'
     # Need to use absolute paths for it to recognise `from Mods import ___`
-    find "$PWD/$mod" -name "*.py" -print0 | xargs -0 mypy
-    flake8 $mod
+    find "$PWD/Mods/$mod" -name "*.py" -print0 | xargs -0 mypy
+    flake8 "Mods/$mod"
 done
