aa19adf2a2560ba85533b16da950e089545613a7
apple1417 <git@apple1417.dev>
2022-09-18 09:39:41 +1200

    port better duplicate name handling to enums library

--- Enums/Readme.md
+++ Enums/Readme.md
@@ -20,6 +20,20 @@ if world_info.NetMode == ENetMode.NM_Client:
     pass
 ```
 
+Most structs just use their struct name directly. There are a few cases where this conflicts, when
+this happens the name is prefixed with the outer object name(s) and underscore seperators - e.g.
+`WillowGame.Behavior_AISetFlight:EFlightMode` and `WillowGame.WillowNavigationHandle:EFlightMode`
+are accessible through `Behavior_AISetFlight_EFlightMode` and `WillowNavigationHandle_EFlightMode`
+respectively.
+
+If need be, you can access the unreal `Enum` object through the `._unreal` attribute.
+
+
 # Changelog
+## Enums v1.1
+- Fixed that if two enums shared a name, only the last would be exposed - they now have a more
+  verbose name, adding their outer object name if needed.
+- Added the `._unreal` field to all enums.
+
 ## Enums v1.0
-Inital Release.
+- Inital Release.
--- Enums/__init__.py
+++ Enums/__init__.py
@@ -1,24 +1,76 @@
 import unrealsdk
 from enum import IntEnum
-from typing import TYPE_CHECKING, Dict, Tuple
+from typing import TYPE_CHECKING, Dict, Set, Tuple, Type
 
 from Mods.ModMenu import ModPriorities, ModTypes, RegisterMod, SDKMod
 
-__version_info__: Tuple[int, ...] = (1, 0)
+__version_info__: Tuple[int, ...] = (1, 1)
 __version__: str = ".".join(map(str, __version_info__))
 
 
-_all_enums = {}
-for _enum in unrealsdk.FindAll("Enum", True):
-    _values = {}
-    _idx = 0
+_all_enums: Dict[str, Type[IntEnum]] = {}
+_known_duplicate_names: Set[str] = set()
+
+
+def _get_enum_name(enum: unrealsdk.UObject) -> str:
+    """
+    Gets the name to use for this enum.
+
+    Will use the base name if it's unique, otherwise prefixes with the outer object name and an
+     underscore - e.g. `NavigationPoint_CheckpointRecord`.
+
+    Args:
+        enum: The enum to get the name of.
+    Returns:
+        The enum's name.
+    """
+    name: str = enum.Name
+    while enum.Name in _known_duplicate_names:
+        # Deliberately not error checking this, so that we'll get an exception if we somehow come
+        #  across two different structs with identical name
+        enum = enum.Outer
+        name = enum.Name + "_" + name
+    return name
+
+
+def _define_enum(enum: unrealsdk.UObject) -> None:
+    """
+    Defines an IntEnum for the given unreal enum.
+
+    Does nothing if it already exists.
+
+    Args:
+        enum: The unreal enum to create an IntEnum of.
+    """
+    # Check for a duplicate name
+    if enum.Name in _all_enums:
+        if _all_enums[enum.Name]._unreal == enum:  # type: ignore
+            return
+        _known_duplicate_names.add(enum.Name)
+        other_enum = _all_enums[enum.Name]._unreal  # type: ignore
+        del _all_enums[enum.Name]
+        _define_enum(other_enum)
+    elif _get_enum_name(enum) in _all_enums:
+        return
+
+    values = {}
+    idx = 0
     while True:
-        _name = _enum.GetEnum(_enum, _idx)
-        if _name == "None":
+        val_name = enum.GetEnum(enum, idx)
+        if val_name == "None":
             break
-        _values[_name] = _idx
-        _idx += 1
-    _all_enums[_enum.Name] = IntEnum(_enum.Name, _values)
+        values[val_name] = idx
+        idx += 1
+
+    int_enum = IntEnum(_get_enum_name(enum), values)  # type: ignore
+    int_enum._unreal = enum  # type: ignore
+
+    _all_enums[_get_enum_name(enum)] = int_enum
+
+
+for _enum in unrealsdk.FindAll("Enum", True):
+    _define_enum(_enum)
+
 
 globals().update(_all_enums)
 __all__ = list(_all_enums.keys())
